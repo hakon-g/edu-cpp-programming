148d44e327954414417b72780b5d60b30dd1eb36
hakon77 <hgjone77@gmail.com>
2017-04-10 23:05:58 +0200

    Added a simple Hello-World app.

---
+++ HelloWorldApp/HelloWorldApp/HelloWorldApp.cpp
@@ -0,0 +1,16 @@
+// HelloWorldApp.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+int main()
+{
+	char input;
+
+	std::cout << "Hello World!" << std::endl;
+	std::cin >> input;
+
+    return 0;
+}
+
